778b1896285b6f997591114ddac61f71567804c2
JessicaLorenzon <jelorenzon@gmail.com>
2025-06-26 18:31:16 -0300

    finalizando projeto

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -25,6 +25,7 @@ public class Controller {
             for (GitHubEvent gitHubEvent : gitHubEvents) {
                 processEvent(gitHubEvent);
             }
+
         } catch (NotFoundException e) {
             view.displayNotFoundExceptionError();
         } catch (Exception e) {
--- src/model/GitHubPayload.java
+++ src/model/GitHubPayload.java
@@ -1,19 +1,12 @@
 package model;
 
-import java.util.List;
-
 public class GitHubPayload {
 
     private Integer size;
     private String ref_type;
-    List<GitHubCommit> gitHubCommits;
 
     public Integer getSize() {
         return size;
-    }
-
-    public List<GitHubCommit> getCommits() {
-        return gitHubCommits;
     }
 
     public String getRefType() {
